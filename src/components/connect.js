b7428d448624255bdd386cfc4f7b939b31e20fbb
Martin <cauu@163.com>
2016-09-30 12:55:32 +0800

    copy utils from react-redux

---
+++ src/components/connect.js
@@ -0,0 +1,5 @@
+export default function connect(mapStateToData, mapDispatchToData, options={}) {
+  const shouldSubscribe = Boolean(mapStateToProps);
+  const mapState = mapStateToProps || defaultMapStateToProps;
+
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Provider from './components/Provider.js';
+import connect from './components/connect.js';
+
+module.exports = {
+  Provider: Provider,
+  connect: connect
+};
